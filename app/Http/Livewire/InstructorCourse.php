874e63a0f9f90c93c21575ee3d552b8153d95ed6
saidnahum <70162272+saidnahum@users.noreply.github.com>
2021-08-11 23:25:51 -0500

    home update

--- app/Http/Livewire/InstructorCourse.php
+++ app/Http/Livewire/InstructorCourse.php
@@ -3,11 +3,17 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Course;
+use Livewire\WithPagination;
 
 class InstructorCourse extends Component
 {
+    use WithPagination;
+
     public function render()
     {
-        return view('livewire.instructor-course');
+        $courses = Course::where('user_id', auth()->user()->id)->paginate(10);
+
+        return view('livewire.instructor-course', compact('courses'));
     }
 }
