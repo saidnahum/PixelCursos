3fd995e22cb7a5af232d121a5d8409fc43166361
saidnahum <saidnahum@gmail.com>
2021-08-12 18:15:11 -0500

    work update

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -10,6 +10,6 @@
 		</div>
 	</section>
 
-	@livewire('course-index')
+	@livewire('courses-index')
 
 </x-app-layout>
--- routes/instructor.php
+++ routes/instructor.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Livewire\InstructorCourse;
+use App\Http\Controllers\Instructor\CourseController;
 
 Route::redirect('', 'instructor/courses');
-Route::get('courses', InstructorCourse::class)->middleware('can:Leer cursos')->name('courses.index');
+
+Route::resource('courses', CourseController::class)->names('courses');
