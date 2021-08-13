25c21bb4ea3a33bfb5ba67ba902ee5d808542695
saidnahum <saidnahum@gmail.com>
2021-08-13 17:31:47 -0500

    Revert "home update"
    This reverts commit 874e63a0f9f90c93c21575ee3d552b8153d95ed6.

--- app/Http/Livewire/InstructorCourse.php
+++ app/Http/Livewire/InstructorCourse.php
@@ -3,17 +3,11 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
-use App\Models\Course;
-use Livewire\WithPagination;
 
 class InstructorCourse extends Component
 {
-    use WithPagination;
-
     public function render()
     {
-        $courses = Course::where('user_id', auth()->user()->id)->paginate(10);
-
-        return view('livewire.instructor-course', compact('courses'));
+        return view('livewire.instructor-course');
     }
 }
